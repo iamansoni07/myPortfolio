404e81d22f79a6fa4804bbea6e8dd49e2ad258c8
iamansoni07 <aman.soni0713@gmail.com>
2024-12-24 10:00:00 +0530

    .

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -45,36 +45,9 @@ const Header = () => (
       </div>
     </motion.div>
 
-    {/* the profile image appears first, followed by the scaling effect of the circle image. */}
-    <motion.div
-      whileInView={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5, delayChildren: 0.5 }}
-      className="app__header-img"
-    >
-      <img src={images.profile} alt="profile_bg" />
-      <motion.img
-        whileInView={{ scale: [0, 1] }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-        src={images.circle}
-        alt="profile_circle"
-        className="overlay_circle"
-      />
-    </motion.div>
 
-    {/* Each circle smoothly scales in and fades into view when the parent component enters the viewport. */}
-    <motion.div
-      variants={scaleVariants}
-      whileInView={scaleVariants.whileInView}
-      className="app__header-circles"
-    >
-      {[images.react, images.javascript2, images.nodejs2].map(
-        (circle, index) => (
-          <div className="circle-cmp app__flex" key={`circle-${index}`}>
-            <img src={circle} alt="profile_bg" />
-          </div>
-        )
-      )}
-    </motion.div>
+
+
   </div>
 );
 
