db96e7426a69424b31907e5a32e472659f9f3719
iamansoni07 <aman.soni0713@gmail.com>
2024-12-29 07:03:58 +0530

    initial commits

--- frontend_react/src/container/index.js
+++ frontend_react/src/container/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import About from './About/About';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Skills from './Skills/Skills';
+import Testimonial from './Testimonial/Testimonial';
+import Work from './Work/Work';
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default index
+export {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work,
+};
